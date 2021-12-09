e4af269748b6a7542b179e469f3ff58c8b2d8d35
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 12:04:58 +0100

    film database

---
+++ 5_Filmova_databaze/5_Filmova_databaze/filmy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_Filmova_databaze
+{
+    class film
+    {
+        public film(string jmeno, string autor, string zanr, int rok)
+        {
+            Jmeno = jmeno;
+            Autor = autor;
+            Zanr = zanr;
+            Rok = rok;
+
+        }
+
+        public string Jmeno { get; set; }
+        public int Rok { get; set; }
+        public string Autor { get; set; }
+        public string Zanr { get; set; }
+    }
+}
