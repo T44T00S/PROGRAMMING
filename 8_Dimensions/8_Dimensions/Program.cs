19d8d0d147714c19e28990e6b21785ba5a08055c
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 11:27:09 +0100

    int setup for 1D, 2D and 3D

--- 8_Dimensions/8_Dimensions/Program.cs
+++ 8_Dimensions/8_Dimensions/Program.cs
@@ -7,8 +7,8 @@ namespace _8_Dimensions
         static void Main(string[] args)
         {
             int[] oned = new int[4];
-            int[,] twod = new int[3, 2] { 5, 9 }, { 3, 0}, { 4, 0} ;
-
+            int[,] twod = new int[3, 2] { { 5, 9 }, { 3, 0 }, { 4, 0 } };
+            int[,,] threed = new int[3, 3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, { {1, 2, 3} };
 
         }
     }
