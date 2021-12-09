c9b3269547a412755739c3334582b5cd575b5fed
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 11:58:55 +0100

    managing projects

---
+++ 6_Bankomat/6_Bankomat/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace _6_Bankomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            user user1 = new user("Honza");
+            user user2 = new user("Karel");
+            user user3 = new user("Anna");
+            List<user> uzivatele = new List<user> { user1, user2, user3 };
+
+            Console.WriteLine(user2.pin);
+
+            bool loggedIn = false;
+
+            while (true)
+            {
+                while (!loggedIn)
+                {
+                    Console.Write("Zadejte jméno: ");
+                    string JMENO = Console.ReadLine();
+
+                    if (uzivatele.Exists(u => u.jmeno == JMENO))
+                    {
+                        Console.WriteLine("Zadejte pin: ");
+                        int PIN = int.Parse(Console.ReadLine());
+
+                        // for (int PIN = 999; PIN < 1000; PIN++)
+                        // {
+
+                        // }
+
+                        if (uzivatele.Find(u => u.jmeno == JMENO).pin == PIN)
+                        {
+                            user uziv = uzivatele.Find(u => u.jmeno == JMENO);
+                            string cmd = "";
+                            do
+                            {
+                                Console.Write("Zadejte příkaz: ");
+                                cmd = Console.ReadLine();
+
+                                switch (cmd)
+                                {
+                                    case "zustatek":
+                                        Console.WriteLine(uziv.zustatek);
+                                        continue;
+
+                                    case "vybrat":
+                                        Console.WriteLine("Kolik peněz si chcete vybrat?");
+                                        uziv.zustatek -= int.Parse(Console.ReadLine());
+                                        continue;
+
+                                    case "odhlasit":
+                                        break;
+
+                                    case "poslat":
+                                        Console.Write("Komu chcete poslat peníze (Honza, Karel, Anna): ");
+                                        string prijemce = Console.ReadLine();
+
+                                        if (uzivatele.Exists(u => u.jmeno == prijemce))
+                                        {
+                                            Console.Write("Jakou částku: ");
+                                            int kolikPoslat = int.Parse(Console.ReadLine());
+
+                                            uziv.zustatek -= kolikPoslat;
+                                            uzivatele.Find(u => u.jmeno == prijemce).zustatek += kolikPoslat;
+                                        }
+                                        continue;
+
+                                    default:
+                                        Console.WriteLine(@"zustatek, vybrat, poslat, odhlasit");
+                                        break;
+                                }
+                            } while (cmd != "odhlasit");
+                        }
+                        else
+                        {
+                            Console.WriteLine("PIN není správný.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Uživatel neexistuje.");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ 6_Bankomat/6_Bankomat/user.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_Bankomat
+{
+    class user
+    {
+        public user(string jmeno)
+        {
+            // Random rd = new Random();
+            //MD5 md5 = MD5.Create();
+
+            this.jmeno = jmeno;
+            //this.pin = md5.ComputeHash(Encoding.ASCII.GetBytes(rd.Next(999, 1000).ToString()));
+            // this.pin = rd.Next(999, 1000);
+            this.pin = 1111;
+            //this.pin = 0000;
+            this.zustatek = 10000;
+        }
+
+        public string jmeno;
+        public int pin;
+        //public byte[] pin;
+        public int zustatek;
+    }
+}
