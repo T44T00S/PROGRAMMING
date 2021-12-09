f965bc39e4482a0467a194deaec29e55d1d67a1d
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 12:23:18 +0100

    metody a vlastnosti (auto database)

---
+++ 3_Metody-a-vlastnosti/3_Metody-a-vlastnosti/Auto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_Metody_a_vlastnosti
+{
+    class Auto
+    {
+        public Auto(string nazev, string typ, string barva)
+        {
+            this.nazev = nazev;
+            this.typ = typ;
+            this.barva = barva;
+        }
+
+        public void UdelatZvuk(string zvuk)
+        {
+            Console.WriteLine(zvuk);
+        }
+
+        public void UpravitTyp()
+        {
+            this.typ += "x";
+        }
+
+        public string nazev { get; set; }
+
+        public string typ { get; set; }
+
+        public string barva { get; set; }
+
+    }
+}
---
+++ 3_Metody-a-vlastnosti/3_Metody-a-vlastnosti/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_Metody_a_vlastnosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto skoda = new Auto("Škoda", "sedan", "Modrá");
+
+            Auto tesla = new Auto("Tesla", "electric", "bílá");
+
+            tesla.UdelatZvuk("vruum");
+
+            Console.WriteLine();
+            Console.WriteLine(skoda.nazev);
+            Console.WriteLine(tesla.nazev);
+            Console.WriteLine(tesla.barva);
+            Console.WriteLine("Název je {0}, jméno je {1}, barva je {2}", skoda.nazev, skoda.typ, skoda.barva);
+
+            Console.ReadLine();
+        }
+    }
+}
