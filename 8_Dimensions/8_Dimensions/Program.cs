8dd3cfcc9052c3a0e7323c2f40bc41d08e81db2a
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 11:22:38 +0100

    dimensions set up

---
+++ 8_Dimensions/8_Dimensions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _8_Dimensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] oned = new int[4];
+            int[,] twod = new int[3, 2] { 5, 9 }, { 3, 0}, { 4, 0} ;
+
+
+        }
+    }
+}
