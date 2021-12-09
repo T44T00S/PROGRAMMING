007bb22948bbc56ef2b2eaf6ba73de21ea35ec8d
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 11:41:02 +0100

    dime

--- 8_Dimensions/8_Dimensions/Program.cs
+++ 8_Dimensions/8_Dimensions/Program.cs
@@ -8,7 +8,7 @@ namespace _8_Dimensions
         {
             int[] oned = new int[4];
             int[,] twod = new int[3, 2] { { 5, 9 }, { 3, 0 }, { 4, 0 } };
-            int[,,] threed = new int[3, 3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, { {1, 2, 3} };
+            int[,,] threed = new int[3, 3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, { {1, 2, 3} { 4, 5, 6} };
 
         }
     }
