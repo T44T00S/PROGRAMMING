f2046691ba4698c91a19a5dd4f24146423bc015d
Petr Martinek <petrklic2005@gmail.com>
2021-12-09 12:45:30 +0100

    work - methods

---
+++ 2_Prace-v-hodine-metody/2_Prace-v-hodine-metody/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2_Prace_v_hodine_metody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ForegroundColor = ConsoleColor.Red;
+            Vozidlo skoda = new Vozidlo("69-LOL", "kabriolet", "modrá");
+
+            Vozidlo tesla = new Vozidlo("55AAGG", "sedan", "bílá");
+
+            tesla.Zatrub("tuuut");
+
+            //Console.WriteLine(tesla.Zatrub + Console.Beep());
+
+            skoda.UtekPolicii();
+            tesla.UtekPolicii();
+
+            //Console.WriteLine(skoda.barva);
+            //Console.WriteLine(tesla.barva);
+            //Console.WriteLine(skoda.spz);
+            Console.WriteLine("Typ auta je {0}, jeho SPZ - {1} a barva je {2}.", skoda.spz, skoda.typ, skoda.barva);
+
+            Console.ReadLine();
+        }
+    }
+}
