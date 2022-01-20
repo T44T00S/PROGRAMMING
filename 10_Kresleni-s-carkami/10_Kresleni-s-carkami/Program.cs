ada2d264eae904015a1d81c45d301e068fef2a97
Petr Martinek <petrklic2005@gmail.com>
2022-01-20 10:40:15 +0100

    making project with figgle

---
+++ 10_Kresleni-s-carkami/10_Kresleni-s-carkami/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Figgle;
+using System.Threading;
+
+namespace _10_Kresleni_s_carkami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ts = new ThreadStart(VypsatCas);
+            var bt = new Thread(ts);
+            bt.Start();
+        }
+        static void VypsatCas()
+        {
+            while (true)
+            {
+                Console.WriteLine(FiggleFonts.Standard.Render($"{ DateTime.Now.Hour}:{ DateTime.Now.Minute}:{ DateTime.Now.Second}"));
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+    }
+}
