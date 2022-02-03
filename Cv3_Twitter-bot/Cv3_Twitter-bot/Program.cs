7ffc9dbeff9108373b746c5568f9945fa9a4ae61
Petr Martinek <petrklic2005@gmail.com>
2022-02-03 10:58:53 +0100

    seting up my new twitter bot project

---
+++ Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cv3_Twitter_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
