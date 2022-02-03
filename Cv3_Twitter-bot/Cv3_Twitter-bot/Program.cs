8a6b9f020a11dc1b1b3c93564a1fc60115100dbf
Petr Martinek <petrklic2005@gmail.com>
2022-02-03 11:30:41 +0100

    further proggress in twitter bot

--- Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
+++ Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Cv3_Twitter_bot
 {
@@ -6,7 +7,12 @@ namespace Cv3_Twitter_bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string APIKey = "1C349dRR86oqNYUXm959a9Y44";
+            string APISecret = "DnSHCRKK5dCzqw31TYSfqF77q1JmYQ9gbz41UrffI8FICeiMyh";
+            string AccessToken = "1205458880593039362-kgWXTvvUfIXazA6JD3MVao9B33KbdN";
+            string AccessSecret = "r449nQ5E09lRtmtzXcr9Fr8tJIbKQoArlULQzEZjKpnIq";
+
+
         }
     }
 }
