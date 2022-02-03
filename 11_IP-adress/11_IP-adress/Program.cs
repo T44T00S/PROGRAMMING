83981975b1fecf28189f589194ca1b8f05227291
Petr Martinek <petrklic2005@gmail.com>
2022-02-03 11:05:58 +0100

    ip and port network in c#

---
+++ 11_IP-adress/11_IP-adress/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _11_IP_adress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress IP = IPAddress.Parse("127.0.0.1");
+            int PORT = 3000;
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IP, PORT));
+
+        }
+    }
+}
