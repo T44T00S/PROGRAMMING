d5d3d9e2a09c4f612d7c7d598f3b34a5aa4bb84c
Petr Martinek <petrklic2005@gmail.com>
2022-02-03 12:47:34 +0100

    trying to make bot work (forbidden error)

--- Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
+++ Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Tweetinvi;
 using Tweetinvi.Models;
 using System.Threading.Tasks;
+using Tweetinvi.Parameters;
 
 namespace Cv3_Twitter_bot
 {
@@ -10,6 +11,23 @@ namespace Cv3_Twitter_bot
     {
         static async Task Main(string[] args)
         {
+            Random RandNum = new Random();
+            string[] SnoopQuotes =
+            {
+                "Gimme the microphone first, so I can bust like a bubble",
+                "Ready to make an entrance, so back on up ('Cause you know we're 'bout to rip shit up)",
+                "Snoop Doggy Dogg and Dr. Dre is at the Door.",
+                "1 2 3 and to the 4.",
+                "Smokin n Coding.",
+                "Buy Low . . . Stay High!!",
+                "PuffPUFFPass",
+                "My mind on my money, my money on my mind.",
+                "Drop it like it’s hot.",
+                "The more medicated the more dedicated.",
+                "You don’t get respect if you don’t deserve it.",
+                "It ain’t no fun if the homies can’t have none."
+            };
+
             string APIKey = "1C349dRR86oqNYUXm959a9Y44";
             string APISecret = "DnSHCRKK5dCzqw31TYSfqF77q1JmYQ9gbz41UrffI8FICeiMyh";
             string AccessToken = "1205458880593039362-kgWXTvvUfIXazA6JD3MVao9B33KbdN";
@@ -19,6 +37,7 @@ namespace Cv3_Twitter_bot
             TwitterClient UserClient = new TwitterClient(APIKey, APISecret, AccessToken, AccessSecret);
 
             IMedia ImageIMedia = await UserClient.Upload.UploadTweetImageAsync(ImageBytes);
+            ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(new PublishTweetParameters(SnoopQuotes[RandNum.Next(10)] + "#ZemanNaUlici #SSPSNotGood #CyberMeansPrestige") { Medias = { ImageIMedia }});
         }
     }
 }
