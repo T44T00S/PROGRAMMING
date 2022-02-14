8669d8dddd582e524a1249acacdf54cca99b5dfd
Petr Martinek <petrklic2005@gmail.com>
2022-02-14 09:24:54 +0100

    last try.out na bot

--- Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
+++ Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
@@ -31,10 +31,13 @@ namespace Cv3_Twitter_bot
             string APIKey = "1C349dRR86oqNYUXm959a9Y44";
             string APISecret = "DnSHCRKK5dCzqw31TYSfqF77q1JmYQ9gbz41UrffI8FICeiMyh";
             string AccessToken = "1205458880593039362-kgWXTvvUfIXazA6JD3MVao9B33KbdN";
-            string AccessSecret = "r449nQ5E09lRtmtzXcr9Fr8tJIbKQoArlULQzEZjKpnIq";
+
+            //string AccessSecret = "r449nQ5E09lRtmtzXcr9Fr8tJIbKQoArlULQzEZjKpnIq";
 
             byte[] ImageBytes = File.ReadAllBytes("snoop.jpg");
-            TwitterClient UserClient = new TwitterClient(APIKey, APISecret, AccessToken, AccessSecret);
+            TwitterClient UserClient = new TwitterClient(APIKey, APISecret);
+
+
 
             IMedia ImageIMedia = await UserClient.Upload.UploadTweetImageAsync(ImageBytes);
             ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(new PublishTweetParameters(SnoopQuotes[RandNum.Next(10)] + "#ZemanNaUlici #SSPSNotGood #CyberMeansPrestige") { Medias = { ImageIMedia }});
