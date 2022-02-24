833f748e120d1751afd69748023a05670d5d40a3
Petr Martinek <petrklic2005@gmail.com>
2022-02-24 10:53:08 +0100

    time and date test c#

---
+++ Cv4_TimeDate_test/Cv4_TimeDate_test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Cv4_TimeDate_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+
+            //2
+            //DateTime Dob;
+            Console.WriteLine("Napiš rok: ");
+            //Dob = DateTime.Parse(Console.ReadLine());
+            int vstup = Int32.Parse(Console.ReadLine());
+            if (vstup > DateTime.Now.Year)
+            {
+                int cas = vstup - DateTime.Now.Year;
+                Console.WriteLine("Zbývající roky: " + cas);
+            }
+
+            //string line = Console.ReadLine();
+            //DateTime dt;
+            //while (!DateTime.TryParseExact(line, "DD/MM/YYYY", null, System.Globalization.DateTimeStyles.None, out dt))
+            //{
+            //    Console.WriteLine("Neplatné datum.");
+            //    line = Console.ReadLine();
+            //}
+
+            //Console.WriteLine($"Do času zbývá: {dt.Year - DateTime.Now.Year}");
+
+            //3
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
