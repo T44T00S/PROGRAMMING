c20c3683c90da93d1705b3dd93516f7ade9b1a2e
Petr Martinek <petrklic2005@gmail.com>
2022-03-03 11:40:14 +0100

    c#

--- Cv5_Smoothie/Cv5_Smoothie/Program.cs
+++ Cv5_Smoothie/Cv5_Smoothie/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cv5_Smoothie
 {
@@ -6,7 +7,12 @@ namespace Cv5_Smoothie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Smoothie sm1 = new Smoothie("Banány", "Jahoda", "Mango");
+            Smoothie sm2 = new Smoothie("Maliny", "Jahoda", "Jablko");
+            Smoothie sm3 = new Smoothie("Jablko", "Borůvky", "Banány");
+            List<Smoothie> smoothies = new List<Smoothie>();
+
+
         }
     }
 }
---
+++ Cv5_Smoothie/Cv5_Smoothie/Smoothie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cv5_Smoothie
+{
+    class Smoothie
+    {
+        public Smoothie(string ing1, string ing2, string ing3)
+        {
+            Ingredient1 = ing1;
+            Ingredient2 = ing2;
+            Ingredient3 = ing3;
+        }
+
+        public string Ingredient1 { get; set; }
+        public string Ingredient2 { get; set; }
+        public string Ingredient3 { get; set; }
+    }
+}
