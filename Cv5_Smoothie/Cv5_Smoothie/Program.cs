2e51c869e4fc20568d2f46a1015da38b18981ad1
Petr Martinek <petrklic2005@gmail.com>
2022-03-03 11:10:43 +0100

    new project - smooothie drink making

---
+++ Cv5_Smoothie/Cv5_Smoothie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cv5_Smoothie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
