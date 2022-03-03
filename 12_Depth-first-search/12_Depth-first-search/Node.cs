01a6521329affa135d5841b55f182a4c34a68010
Petr Martinek <petrklic2005@gmail.com>
2022-03-03 10:37:30 +0100

    c#

--- 12_Depth-first-search/12_Depth-first-search/Node.cs
+++ 12_Depth-first-search/12_Depth-first-search/Node.cs
@@ -6,5 +6,25 @@ namespace _12_Depth_first_search
 {
     class Node
     {
+        public Node(int id)
+        {
+            this.id = id;
+        }
+
+        static void DFS(Node n)
+        {
+            foreach (var item in n.sousedi)
+            {
+                if (item.id == konec)
+                {
+
+                }
+            }
+        }
+
+        private static List<Node> fronta;
+        static int konec;
+        public int id;
+        public List<Node> sousedi;
     }
 }
--- 12_Depth-first-search/12_Depth-first-search/Program.cs
+++ 12_Depth-first-search/12_Depth-first-search/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _12_Depth_first_search
 {
@@ -6,7 +7,36 @@ namespace _12_Depth_first_search
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Node> nody = new List<Node>();
+
+            nody.Add(new Node(1));
+            nody.Add(new Node(2));
+            nody.Add(new Node(3));
+            nody.Add(new Node(4));
+            nody.Add(new Node(5));
+            nody.Add(new Node(6));
+            nody.Add(new Node(7));
+
+            nody.Find(m => m.id == 1).sousedi.Add();
+
+            //NajitNode(1).sousedi.Add(NajitNode(1));
+            //NajitNode(4).sousedi.Add(NajitNode(2));
+            //NajitNode(4).sousedi.Add(NajitNode(3));
+            //NajitNode(4).sousedi.Add(NajitNode(4));
+            //NajitNode(4).sousedi.Add(NajitNode(5));
+            //NajitNode(4).sousedi.Add(NajitNode(6));
+
+            PridatSouseda(1, 4);
+            PridatSouseda(4, 2);
+            PridatSouseda(4, 6);
+            PridatSouseda(2, 1);
+            PridatSouseda(2, 3);
+            PridatSouseda(2, 3);
+
+            void PridatSouseda(int x, int y)
+            {
+                nody.Find(m => m.id == x).sousedi.Add(nody.Find(m => m.id == x));
+            }
         }
     }
 }
