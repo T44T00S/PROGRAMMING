3d857a7218bd471f5a2e1bf52470e1557a0eb6a2
Petr Martinek <petrklic2005@gmail.com>
2022-05-12 10:19:58 +0200

    unknown commit

---
+++ 13_List-studentu/13_List-studentu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _13_List_studentu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Student
+    {
+        public Student 
+    }
+}
