18bfd6da96fd8cac3182643f0bd61636df14a724
T44T00S <petrklic2005@gmail.com>
2022-06-14 21:23:28 +0200

    fix

--- Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
+++ Cv3_Twitter-bot/Cv3_Twitter-bot/Program.cs
@@ -28,9 +28,9 @@ namespace Cv3_Twitter_bot
                 "It ain’t no fun if the homies can’t have none."
             };
 
-            string APIKey = "1C349dRR86oqNYUXm959a9Y44";
-            string APISecret = "DnSHCRKK5dCzqw31TYSfqF77q1JmYQ9gbz41UrffI8FICeiMyh";
-            string AccessToken = "1205458880593039362-kgWXTvvUfIXazA6JD3MVao9B33KbdN";
+            string APIKey = "<>";
+            string APISecret = "<>";
+            string AccessToken = "<>-<>";
 
             //string AccessSecret = "r449nQ5E09lRtmtzXcr9Fr8tJIbKQoArlULQzEZjKpnIq";
 
